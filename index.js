4ab3541badca2ddec2a25eca5689449006fa126e
Piotr Lewandowski <piotrl@Piotrs-MacBook-Pro.local>
2022-01-21 15:00:02 -0500

    Prob 1 finished

--- index.js
+++ index.js
@@ -11,21 +11,3 @@ function convert(number, exRate, currency1, currency2){
 // 1 usd = 0.88 euro
 
 convert(pesos, 20.45, "USD", "Pesos")
-
-//Problem #2
-
-const tempC = 8.5 
-
-
-//const tempF = 9.5
-
-//(0°C × 9/5) + 32 = 32°F
-
-function convert(number){
-    
-    let output = (number * conversion)
-
-    console.log(`${number} degrees celsius convert to ${output} degrees fahrenheit `)
-}
-
-convert(tempC) 
